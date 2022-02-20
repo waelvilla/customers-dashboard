4520b9867af75203cb48a9d66296d9c9b6af2d7e
Wael A <waelvilla@gmail.com>
2022-02-20 12:49:47 +0300

    Rename redux folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { store } from 'src/app/store';
+import { store } from 'src/redux/store';
 import RootNavigator from 'src/navigation';
 
 function App() {
